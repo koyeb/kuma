ebddecb3af356b618b93936472b9846d7cb2e21a
nicoche <78445450+nicoche@users.noreply.github.com>
2024-02-06 10:03:56 +0100

    Add debug logs to dns vips

--- pkg/dns/vips/virtual_outbound_view.go
+++ pkg/dns/vips/virtual_outbound_view.go
@@ -2,6 +2,7 @@ package vips
 
 import (
 	"fmt"
+	"errors"
 	"net"
 	"sort"
 
@@ -36,6 +37,8 @@ func (vo *VirtualOutboundMeshView) Get(entry HostnameEntry) *VirtualOutbound {
 	return vo.byHostname[entry]
 }
 
+var CustomErr error = errors.New("triggered")
+
 func (vo *VirtualOutboundMeshView) Add(entry HostnameEntry, outbound OutboundEntry) error {
 	if vo.byHostname[entry] == nil {
 		vo.byHostname[entry] = &VirtualOutbound{Outbounds: []OutboundEntry{outbound}}
@@ -55,7 +58,7 @@ func (vo *VirtualOutboundMeshView) Add(entry HostnameEntry, outbound OutboundEnt
 				return nil
 			} else {
 				logger.Error(fmt.Errorf("can't add %s:%d from %s because it's already used by entity defined in:'%s'", entry.Name, outbound.Port, outbound.Origin, existingOutbound.Origin), "failed to generate DNS entry")
-				return nil
+				return CustomErr
 			}
 		}
 	}
--- pkg/dns/vips_allocator.go
+++ pkg/dns/vips_allocator.go
@@ -390,7 +390,12 @@ func (d *VIPsAllocator) buildVirtualOutboundMeshView(
 				Origin: vips.OriginHost(es.GetMeta().GetName()),
 			})
 			if addError != nil {
-				errs = multierr.Append(errs, errors.Wrapf(addError, "cannot add outbound for external service '%s'", es.GetMeta().GetName()))
+				if errors.Is(addError, vips.CustomErr) {
+					Log.WithValues("externlService", es.GetMeta().GetName()).
+						Info("failed to call Add()", "reason", addError.Error())
+				} else {
+					errs = multierr.Append(errs, errors.Wrapf(addError, "cannot add outbound for external service '%s'", es.GetMeta().GetName()))
+				}
 			}
 		}
 		for _, vob := range Match(virtualOutbounds, tags) {
@@ -482,14 +487,24 @@ func addFromVirtualOutbound(outboundSet *vips.VirtualOutboundMeshView, vob *core
 		Origin: vips.OriginVirtualOutbound(vob.Meta.GetName()),
 	})
 	if err != nil {
-		l.Info("Failed adding generated outbound", "reason", err.Error())
+		l.Info("Failed adding generated outbound in addFromVirtualOutbound", "reason", err.Error())
 	}
 }
 
 func addDefault(outboundSet *vips.VirtualOutboundMeshView, service string, port uint32) error {
-	return outboundSet.Add(vips.NewServiceEntry(service), vips.OutboundEntry{
+	err := outboundSet.Add(vips.NewServiceEntry(service), vips.OutboundEntry{
 		TagSet: map[string]string{mesh_proto.ServiceTag: service},
 		Origin: vips.OriginService,
 		Port:   port,
 	})
+	if err != nil {
+		if errors.Is(err, vips.CustomErr) {
+			Log.Error(err, "failed to call Add()", "outboundSet", outboundSet, "service", service, "port", port)
+			return nil
+		}
+
+		return err
+	}
+
+	return nil
 }
