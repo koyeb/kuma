89dd6ba27409e92ed6b87b5b60021a8284f31b5b
nicoche <78445450+nicoche@users.noreply.github.com>
2024-03-26 17:32:07 +0100

    gob: register more types

--- api/mesh/v1alpha1/init.go
+++ api/mesh/v1alpha1/init.go
@@ -7,4 +7,8 @@ import (
 func init() {
 	gob.Register(&TrafficRoute_LoadBalancer_RoundRobin_{})
 	gob.Register(&ProxyTemplate_Modifications_Listener_{})
+	gob.Register(&ProxyTemplate_Modifications_Cluster_{})
+	gob.Register(&ProxyTemplate_Modifications_NetworkFilter_{})
+	gob.Register(&ProxyTemplate_Modifications_HttpFilter_{})
+	gob.Register(&ProxyTemplate_Modifications_VirtualHost_{})
 }
